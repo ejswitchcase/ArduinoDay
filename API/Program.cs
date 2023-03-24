1478f9e5fa9d341aca31088884a2504e757ca2ad
Davi Freitas <davimello14@gmail.com>
2023-03-24 16:56:46 -0300

    Update api

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using API.Context;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<EnergyDbContext>(c => c.UseMySql("Server=localhost;UserID = root;Password=root;Database=visudata;", ServerVersion.Parse("8.0.30-mysql")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
